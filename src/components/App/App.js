fbd12a164d37b6f3431b9a928b0a914494d4c3ef
Daria Ovchinnikova <daria_ovch@mail.ru>
2021-02-23 23:11:10 +0100

    created app & main & card components & added proptypes validation

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from '../Main/Main.js';
+
+function App() {
+  return (
+    <>
+      <Main title="The Grand Budapest Hotel" genre="Drama" year="2014" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Card/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Card({item}) {
+
+  return (
+    <article className="small-movie-card catalog__movies-card">
+      <div className="small-movie-card__image">
+        <img src={item.image} alt={item.alt} width="280" height="175" />
+      </div>
+      <h3 className="small-movie-card__title">
+        <a className="small-movie-card__link" href="movie-page.html">{item.title}</a>
+      </h3>
+    </article>
+  );
+}
+
+Card.propTypes = {
+  item: PropTypes.object,
+};
+
+export default Card;
