f0da12f66d60a2ce02174635f94b0f27fe2ce59c
Daria Ovchinnikova <daria_ovch@mail.ru>
2021-03-01 17:36:05 +0100

    inline for logo classname

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,11 +4,9 @@ import cn from 'classnames';
 
 function Logo({theme}) {
 
-  const logoClassName = cn(`logo__link`, {'logo__link--light': theme === `light`});
-
   return (
     <div className="logo">
-      <a className={logoClassName}>
+      <a className={cn(`logo__link`, {'logo__link--light': theme === `light`})}>
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
