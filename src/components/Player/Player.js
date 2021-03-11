3f0f683054be57a82970be777866835df24248ec
Daria Ovchinnikova <daria_ovch@mail.ru>
2021-03-11 17:50:51 +0100

    added pause for player

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -74,10 +74,19 @@ function Player({movies}) {
 
           <div className="player__controls-row">
             <button type="button" className="player__play" onClick={handlePlayClick}>
-              <svg viewBox="0 0 19 19" width="19" height="19">
-                <use xlinkHref="#play-s"></use>
-              </svg>
-              <span>Play</span>
+              {!isPlaying && <>
+                <svg viewBox="0 0 19 19" width="19" height="19">
+                  <use xlinkHref="#play-s"></use>
+                </svg>
+                <span>Play</span>
+              </>}
+
+              {isPlaying && <>
+                <svg viewBox="0 0 14 21" width="14" height="21">
+                  <use xlinkHref="#pause"></use>
+                </svg>
+                <span>Pause</span>
+              </>}
             </button>
             <div className="player__name">Transpotting</div>
 
